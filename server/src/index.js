0b7517c237de9fdfc72b9d4db212c9f43ec487f7
iHuzaifaEhsan <iamhuzaifaehsan@gmail.com>
2025-07-23 12:24:50 +0500

    Cors error resolved

--- server/src/index.js
+++ server/src/index.js
@@ -9,11 +9,22 @@ dbConnect();
 const app = express();
 
 // All frontend from port 5173
-app.use(cors({
-  origin: 'http://localhost:5173',
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
-}));
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://rupexo-server.vercel.app",
+];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 
 // Middlewares
 app.use(express.json());
