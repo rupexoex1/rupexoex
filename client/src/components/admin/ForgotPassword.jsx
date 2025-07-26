75d6819f75e4c081de118d1ef3caa69909e22e73
iHuzaifaEhsan <iamhuzaifaehsan@gmail.com>
2025-07-26 18:47:23 +0500

    Fixed some UI bugs

--- client/src/components/admin/ForgotPassword.jsx
+++ client/src/components/admin/ForgotPassword.jsx
@@ -28,7 +28,7 @@ const ForgotPassword = () => {
   };
 
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex items-center justify-center h-[calc(100vh-4rem)]">
       <form
         onSubmit={handleSendOtp}
         className="bg-white shadow-md rounded px-8 pt-6 pb-8 w-full max-w-sm"
--- client/src/components/admin/ResetPassword.jsx
+++ client/src/components/admin/ResetPassword.jsx
@@ -42,7 +42,7 @@ const ResetPassword = () => {
   };
 
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex items-center justify-center h-[calc(100vh-4rem)]">
       <form
         onSubmit={handleReset}
         className="bg-white shadow-md rounded px-8 pt-6 pb-8 w-full max-w-sm"
