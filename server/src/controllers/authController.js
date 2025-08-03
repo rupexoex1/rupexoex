09ae5622c6d87ea037b10ab8b416b7df93ff96c4
iHuzaifaEhsan <iamhuzaifaehsan@gmail.com>
2025-08-03 10:02:33 +0500

    Session Expiry Removed of 1h

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -191,7 +191,6 @@ export const login = async (req, res) => {
     const token = jwt.sign(
       { id: user._id, role: user.role },
       process.env.JWT_SECRET,
-      { expiresIn: "1h" } // Optional: Set expiration time for the token
     );
 
     // Send response with user details and token
