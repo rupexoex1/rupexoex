7e882db1d0462eee3069991faafc872f36fbbc37
rupexoex <rupexoex@gmail.com>
2025-08-19 11:02:28 +0500

    Backend Autopush serveravatar

--- server/src/index.js
+++ server/src/index.js
@@ -40,5 +40,5 @@ app.use("/api/v1/users", userRoutes);
 // Start the server
 const PORT = process.env.PORT || 7002;
 app.listen(PORT, () => {
-  console.log(`Server is running at port ${PORT}`);
+  console.log(`Server is running at port: ${PORT}`);
 });
