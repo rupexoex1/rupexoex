d67d9c8c7a9989e6e2ae2ea8f5d71646d7c1d509
rupexoex1 <rpx.server1@gmail.com>
2025-09-30 18:46:28 +0500

    Backend

--- server/src/index.js
+++ server/src/index.js
@@ -17,7 +17,8 @@ const allowedOrigins = [
   "https://rupexo.com",
   "https://www.rupexo.com",
   "https://rupexo-server-phi.vercel.app",
-  "https://rupexo-one.vercel.app"
+  "https://rupexo-one.vercel.app",
+  "https://rupexo-backend.vercel.app"
 ];
 
 app.use(cors({
