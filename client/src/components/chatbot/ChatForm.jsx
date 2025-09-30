318e81830f3c3ce3c2e9e4234f83cbcbc0733153
rupexoex1 <rpx.server1@gmail.com>
2025-09-30 19:26:15 +0500

    removed chatbot

--- client/src/components/chatbot/ChatForm.jsx
+++ client/src/components/chatbot/ChatForm.jsx
@@ -1,33 +1,33 @@
-import React, { useRef } from 'react'
+// import React, { useRef } from 'react'
 
-const ChatForm = ({ chatHistory, setChatHistory, generateBotResponse }) => {
+// const ChatForm = ({ chatHistory, setChatHistory, generateBotResponse }) => {
 
-  const inputRef = useRef()
+//   const inputRef = useRef()
 
-  const handleFormSubmit = (e) => {
-    e.preventDefault();
-    const userMessage = inputRef.current.value.trim()
-    if (!userMessage) return;
-    inputRef.current.value = '';
+//   const handleFormSubmit = (e) => {
+//     e.preventDefault();
+//     const userMessage = inputRef.current.value.trim()
+//     if (!userMessage) return;
+//     inputRef.current.value = '';
 
-    // Update chat history with the user's message
-    setChatHistory((history) => [...history, { role: "user", text: userMessage }])
+//     // Update chat history with the user's message
+//     setChatHistory((history) => [...history, { role: "user", text: userMessage }])
 
-    // Delay 600 ms before showing "Thinking..." and generating response
-    setTimeout(() => {
-      // Add a "Thinking..." placeholder for the bot's response
-      setChatHistory((history) => [...history, { role: "model", text: "Thinking..." }]);
-      generateBotResponse([...chatHistory, { role: "user", text: `Using the details provided above, please address this query: ${userMessage}` }])
-    }, 600)
-  }
-  return (
-    <form action="#" className='chat-form' onSubmit={handleFormSubmit}>
-      <input ref={inputRef} type="text" placeholder='Message...' className='message-input text-black' required />
-      <button className="material-symbols-rounded">
-        arrow_upward
-      </button>
-    </form>
-  )
-}
+//     // Delay 600 ms before showing "Thinking..." and generating response
+//     setTimeout(() => {
+//       // Add a "Thinking..." placeholder for the bot's response
+//       setChatHistory((history) => [...history, { role: "model", text: "Thinking..." }]);
+//       generateBotResponse([...chatHistory, { role: "user", text: `Using the details provided above, please address this query: ${userMessage}` }])
+//     }, 600)
+//   }
+//   return (
+//     <form action="#" className='chat-form' onSubmit={handleFormSubmit}>
+//       <input ref={inputRef} type="text" placeholder='Message...' className='message-input text-black' required />
+//       <button className="material-symbols-rounded">
+//         arrow_upward
+//       </button>
+//     </form>
+//   )
+// }
 
-export default ChatForm
+// export default ChatForm
--- client/src/components/chatbot/ChatMessage.jsx
+++ client/src/components/chatbot/ChatMessage.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import ChatbotIcon from './ChatbotIcon'
+// import React from 'react'
+// import ChatbotIcon from './ChatbotIcon'
 
-const ChatMessage = ({ chat }) => {
-  return (
-    !chat.hideInChat && (
-      <div className={`message ${chat.role === 'model' ? 'bot' : 'user'}-message ${chat.isError ? "error" : ""}`}>
-        {chat.role === "model" && <ChatbotIcon />}
-        <p className='message-text'>
-          {chat.text}
-        </p>
-      </div>
-    )
-  )
-}
+// const ChatMessage = ({ chat }) => {
+//   return (
+//     !chat.hideInChat && (
+//       <div className={`message ${chat.role === 'model' ? 'bot' : 'user'}-message ${chat.isError ? "error" : ""}`}>
+//         {chat.role === "model" && <ChatbotIcon />}
+//         <p className='message-text'>
+//           {chat.text}
+//         </p>
+//       </div>
+//     )
+//   )
+// }
 
-export default ChatMessage
+// export default ChatMessage
